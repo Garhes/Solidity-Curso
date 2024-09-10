080bdb5448c51771f8249e80089951ed46616d71
Garhes <Johangarcia101999@gmail.com>
2024-09-09 19:51:27 -0600

    se hace el primer contrato en avalachance

---
+++ Hola_mundo.sol
@@ -0,0 +1,8 @@
+// SPDX-license-Indentifier: Mit
+// Primer contrato en Hola Mundo 
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+contract Hola_Mundo{
+    string public Saludo = "Hola Mundo desde mi primer curso de Avalanche";
+}
