df7a8e756b46c49d55a98a2dd5480f3b30a9a23f
Garhes <Johangarcia101999@gmail.com>
2024-09-13 21:55:07 -0600

    tarea

--- tarea_solidity.sol
+++ tarea_solidity.sol
@@ -11,7 +11,7 @@ y mi telefono en formato de numero.
 */
 pragma solidity 0.8.24;
 contract tarea_solidity{
-    //creamos variables 
+    //creamos la variable y el boton de nombre completo 
     string private fullname;
     constructor(string memory nombre_completo) {
         fullname = string.concat("Tu Nombre completo es:", " ", nombre_completo); 
@@ -20,7 +20,8 @@ contract tarea_solidity{
     function hola_name()public view returns(string memory) {
         return fullname;
     }
-
+    // creamos la variable y un boton dinamico del correo al igual que del numero que cuando digiten 
+    // un dato este los actualice y los guarde de una vez 
     string correo = "Tu correo es:";
 
 	function leer_correo() public view returns(string memory){
